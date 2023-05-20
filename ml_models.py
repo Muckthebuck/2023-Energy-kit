200926b356d7f398feadc3955269e57b6ba2ca82
mchodhary <mchodhary@student.unimelb.edu.au>
2023-05-20 14:43:00 +1000

    updated ml_models to output moving average of predicted values

--- ml_models.py
+++ ml_models.py
@@ -1,9 +1,11 @@
 import tensorflow as tf
 from online_batches import online_batches
+import pandas as pd
+import numpy as np
 
 class ml_model:
     
-    def __init__(self) -> None:
+    def __init__(self, data_batches) -> None:
         self.solar_model = tf.keras.models.load_model("saved_model/temp_Austin_solar_lstm3/")
         self.temp_model = tf.keras.models.load_model("saved_model/temp_Austin_temp_lstm3/")
         self.load_model = None
@@ -21,22 +23,40 @@ class ml_model:
         self.austin_N = 6   # number of features
         self.load_N = 1     # number of load features
         self.LR = 1e-3      # learning rate of the gradient descent
+        self.labels = data_batches.get_timestamps()
+        self.temp_avg = pd.DataFrame({'predicted':  np.full(self.labels.shape[0], np.nan)},index=self.labels)
+        self.solar_avg = pd.DataFrame({'predicted':  np.full(self.labels.shape[0], np.nan)},index=self.labels)
+        self.load_avg = pd.DataFrame({'predicted':  np.full(self.labels.shape[0], np.nan)},index=self.labels)
        
-
     def predict(self, t, data_batches: online_batches):
         # Call predict method on the loaded model
         temp_X, temp_y, temp_scalar, solar_X, solar_y, solar_scalar, load_X, load_y, load_scalar = data_batches.get_online_training_data(t)
-        temp_hat = self._predict(self.temp_model, temp_X, temp_y,self.austin_N, temp_scalar)
-        solar_hat = self._predict(self.solar_model, solar_X, solar_y,self.austin_N, solar_scalar)
+        temp_hat = self._predict(self.temp_model, temp_X, temp_y,self.austin_N, temp_scalar,self.temp_avg, t, 'temp')
+        solar_hat = self._predict(self.solar_model, solar_X, solar_y,self.austin_N, solar_scalar, self.solar_avg, t, 'solar')
+        # load_hat = self._predict(self.load_model, load_X, load_y,self.load_N, load_scalar, self.load_avg, t, 'load')
         load_hat = 0
-        # load_hat = self._predict(self.load_model, t,load_X, load_y,self.load_N, load_scalar)
+
         # need to divide solar by 1000 to conver to K(units)
         return temp_hat,solar_hat/1000,load_hat
     
-    def _predict(self,model, X_test, y_test, N, scalar):
+    def _predict(self,model, X_test, y_test, N, scalar, avg_df, i, which):
         x = X_test.reshape(-1,self.T,N)  # a "new" input is available
         y_hat = model.predict_on_batch(x) # predict on the "new" input
         # averaged = np.mean(y_hat[:,3:6,:], axis=1)
+
         y = y_test.reshape(1,-1,1)   # a "new" label is available
         model.train_on_batch(x, y)  # runs a single gradient update 
-        return scalar.inverse_transform(y_hat)
+
+        predicted = scalar.inverse_transform(y_hat)
+       
+        index = self.labels[i:i+y_test.shape[0]]
+       
+        dff1 = pd.DataFrame({'predicted': predicted.squeeze()}, index=index)
+        avg_df = avg_df.join(dff1, how='left', rsuffix=f'_{i}')
+        if(which=='solar'):
+            self.solar_avg = avg_df
+        elif(which=='temp'):
+            self.temp_avg = avg_df
+        dff_avg = avg_df.iloc[i]
+        predicted = dff_avg.mean()
+        return predicted
